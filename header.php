75c8247110b3dcef64c10d640354ac5f71d015e0
Higor <higor@maxmeio.com>
2012-10-04 10:07:07 -0300

    adjusts

--- header.php
+++ header.php
@@ -4,37 +4,36 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
 	<title><?php
-	/*
-	* Print the <title> tag based on what is being viewed.
-	*/
-	global $page, $paged;
+    	/*
+    	* Print the <title> tag based on what is being viewed.
+    	*/
+    	global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+    	wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
+    	// Add the blog name.
+    	bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+    	// Add the blog description for the home/front page.
+    	$site_description = get_bloginfo( 'description', 'display' );
+    	if ( $site_description && ( is_home() || is_front_page() ) )
+    		echo " | $site_description";
 
-	// Add a page number if necessary:
+    	// Add a page number if necessary:
 		if ( $paged >= 2 || $page >= 2 )
 			echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
 
 		?></title>
-		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/lib/css/boilerplate.css?v=2"/>
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/lib/css/common.css"/>
+        <link rel="stylesheet/less" type="text/css" href="<?php bloginfo('template_url') ?>/lib/css/styles.less">
+        <script src="<?php bloginfo('template_url') ?>/lib/js/less-1.3.0.min.js" type="text/javascript"></script>
+        
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 		<?php wp_head() ?>
-		
-		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-        <script type="text/javascript">stLight.options({publisher: "7fb3916b-de05-45b9-b957-e05c00a4555d"}); </script>
 	</head>
 
-	<body >
+	<body>
 		
         <div class="page center">
             <!-- ======================================================================================= -->
